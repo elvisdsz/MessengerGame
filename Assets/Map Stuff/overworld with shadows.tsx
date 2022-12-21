e6192b9e4e0ff4849d35573d2edcc9b044dfce28
coleraffell <cole_raffell@outlook.com>
2022-12-21 16:37:58 +0000

    map start

---
+++ Assets/Map Stuff/overworld with shadows.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="overworld with shadows" tilewidth="16" tileheight="16" spacing="1" tilecount="119" columns="17">
+ <image source="overworld with shadows.png" width="304" height="128"/>
+</tileset>
