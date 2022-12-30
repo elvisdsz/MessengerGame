56f24a1c6dc185c4887dae055e2a26339c8d14a3
coleraffell <cole_raffell@outlook.com>
2022-12-30 13:30:37 +0000

    finished outer map and camp

---
+++ Assets/Map Stuff/Mystic forest pack/water_lillies.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="water_lillies" tilewidth="16" tileheight="16" tilecount="6" columns="6">
+ <image source="../sprites/tilesets/water_lillies.png" width="96" height="16"/>
+</tileset>
