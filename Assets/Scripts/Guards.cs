9ef646680a10dcfb0b8d42ce18062160900cb776
coleraffell <cole_raffell@outlook.com>
2023-01-10 16:52:06 +0000

    a

---
+++ Assets/Scripts/Guards.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guards : MonoBehaviour
+{
+
+    void Start() {
+        DialogueSystem._instance.ClearDialogues();
+
+    }
+}
