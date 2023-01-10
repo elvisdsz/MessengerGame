44111b799772e226be8fc937c80e181c55308ade
Elvis Dsouza <elvis.dsouza666@gmail.com>
2023-01-10 16:07:41 +0000

    Hide arrow error in console

--- Assets/Scripts/NarrativeGuide.cs
+++ Assets/Scripts/NarrativeGuide.cs
@@ -56,27 +56,29 @@ public class NarrativeGuide : MonoBehaviour {
         }
         */
         
+        try {
+            if(interestTransform == null)
+                pointer.gameObject.SetActive(false);
 
-        if(interestTransform == null)
-            pointer.gameObject.SetActive(false);
+            screenPos = _camera.WorldToViewportPoint(interestTransform.position); //get viewport positions
+    
+            if(screenPos.x >= 0 && screenPos.x <= 1 && screenPos.y >= 0 && screenPos.y <= 1){
+                pointer.gameObject.SetActive(false);
+                return;
+            }
 
-        screenPos = _camera.WorldToViewportPoint(interestTransform.position); //get viewport positions
- 
-        if(screenPos.x >= 0 && screenPos.x <= 1 && screenPos.y >= 0 && screenPos.y <= 1){
-            pointer.gameObject.SetActive(false);
-            return;
+            pointer.gameObject.SetActive(true);
+            onScreenPos = new Vector2(screenPos.x-0.5f, screenPos.y-0.5f)*2; //2D version, new mapping
+            max = Mathf.Max(Mathf.Abs(onScreenPos.x), Mathf.Abs(onScreenPos.y)); //get largest offset
+            onScreenPos = (onScreenPos/(max*2))+new Vector2(0.5f, 0.5f); //undo mapping
+            //Debug.Log(onScreenPos);
+            Vector3 centre = _camera.ViewportToScreenPoint(new Vector2(0.5f, 0.5f));
+            Vector3 ptrPos = _camera.ViewportToScreenPoint(onScreenPos);
+            Vector3 directionAway = ptrPos - centre;
+            pointer.position = ptrPos - (0.1f*directionAway);
+            pointer.transform.rotation = Quaternion.Euler(0f, 0f, Vector2.SignedAngle(Vector2.up, directionAway));
+        } catch {
         }
-
-        pointer.gameObject.SetActive(true);
-        onScreenPos = new Vector2(screenPos.x-0.5f, screenPos.y-0.5f)*2; //2D version, new mapping
-        max = Mathf.Max(Mathf.Abs(onScreenPos.x), Mathf.Abs(onScreenPos.y)); //get largest offset
-        onScreenPos = (onScreenPos/(max*2))+new Vector2(0.5f, 0.5f); //undo mapping
-        //Debug.Log(onScreenPos);
-        Vector3 centre = _camera.ViewportToScreenPoint(new Vector2(0.5f, 0.5f));
-        Vector3 ptrPos = _camera.ViewportToScreenPoint(onScreenPos);
-        Vector3 directionAway = ptrPos - centre;
-        pointer.position = ptrPos - (0.1f*directionAway);
-        pointer.transform.rotation = Quaternion.Euler(0f, 0f, Vector2.SignedAngle(Vector2.up, directionAway));
     }
 
     public void SetInterestTransform(Transform focusTransform) {
